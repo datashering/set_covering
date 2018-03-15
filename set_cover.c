f9063862d0305fea4364353ee08e4a68ded3f036
vinicius_tm <vinicius.t.mello.1@gmail.com>
2018-03-15 20:27:34 -0300

    Codigo comentado

--- set_cover.c
+++ set_cover.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 #include <glpk.h>
 #include <math.h>
-#define D2R 3.14159265358979323846/180
 
+#define D2R 3.14159265358979323846/180
+#define R_MAX = 300
+
+/*Estrutura de dados que guarda as informacoes do .dat*/
 typedef struct{
-  int n;
-  int *c;
-  int **a;
+  int n;    // Número de localidades
+  int *c;   // Vetor com custos de abrir cada facilidade
+  int **a;  // Matrix binaria onde 1-> se a localidade j está dentro do raio de cobertura da localidade j
 }Data;
 
+/*Função que aplica que calcula a distancia esferica entre duas localidades
+n -> Número de localidades
+lat1, lng1 -> Latitude e longitude da primeira facilidade
+lat2, lng2 -> Latitude e longitude da segunda facilidade */
 float calc_dist(int n, float lat1, float lng1, float lat2, float lng2){
   float alpha;
   float dist[n][n];
@@ -20,9 +27,14 @@ float calc_dist(int n, float lat1, float lng1, float lat2, float lng2){
 
 }
 
-Data le_arquivo(char f_nome){
+/*Função que lê o arquivo de dados e retorna uma struct com os parametros necessários para resolução do
+problema MIP
+f_nome -> Nome do arquivo .dat*/
+Data le_arquivo(char f_name){
   Data data;
-  FILE *f = fopen(f_nome,"r");
+  FILE *f = fopen(f_name,"r");
+
+  /*Alocacao dinamica de memoria*/
   fscanf(f,"%d",&data.n);
   data.c = (int*) malloc (data.n * sizeof(int));
   data.a = (int**) malloc (data.n*sizeof(int*));
@@ -32,15 +44,22 @@ Data le_arquivo(char f_nome){
   float *lat = (float*) malloc (data.n * sizeof(float));
   float *lng = (float*) malloc (data.n * sizeof(float));
 
+  /*Leitura do arquivo*/
   for(int i=0; i < data.n;i++){
     fscanf(f,"%d ",&data.c[i]);
   }
   for(int i=0; i <data.n; i++){
     fscanf(f,"%f,%f ",&lat[i],&lng[i]);
   }
+  /*Geração da matrix de cobertura a com base na distancia esferica*/
   for(int i=0; i < data.n; i++){
     for(int j = 0; j < data.n; j++){
-      data.a[i][j] = calc_dist(data.n,lat[i],lng[i], lat[j],lng[j]);
+      if(calc_dist(data.n,lat[i],lng[i], lat[j],lng[j]) <= R_MAX){
+        data.a[i][j] == 1;
+      }
+      else{
+        data.a[i][j] == 0;
+      }
     }
   }
   free(lat);
@@ -48,45 +67,52 @@ Data le_arquivo(char f_nome){
   return data;
 }
 
-int main(void)
-{
-  /* declare variables */
+int main(void){
+  /*Declaracao de variaveis*/
   glp_prob *lp;
-  int ia[1+1000], ja[1+1000];
-  char f_nome[]= "sv.dat";
   Data data = le_arquivo(f);
+  int ia[1+1000], ja[1+1000];
   int *x = (int*) malloc ((data.n+1) * sizeof(int));
-  double ar[1+1000], z;
   int aux;
-  /* create problem */
+  double ar[1+1000], z;
+  char f_nome[]= "sc.dat";
+
+  /* Criacao do priblema */
   lp = glp_create_prob();
   glp_set_prob_name(lp, "set_covering");
   glp_set_obj_dir(lp, GLP_MIN);
-  /* fill problem */
+
+  /* Adicionando linhas (restricoes) */
   glp_add_rows(lp, data.n);
-  for (int aux=1; aux <= data.n; aux++){
+  for (int aux=1; aux<=data.n; aux++){
     //glp_set_row_name(lp, aux, "r%d");
     glp_set_row_bnds(lp, aux, GLP_LO,1.0,0.0);
   }
+  /* Adicionando columnas (variaveis) */
   glp_add_cols(lp,data.n);
-  for (int aux=1; aux <= data.n; aux++){
+  for (int aux=1; aux<=data.n; aux++){
     //glp_set_col_name(lp, aux, "x%d");
     glp_set_col_kind(lp, aux, GLP_BV);
   }
-  for(int aux=1; aux <= data.n; aux++){
+
+  /* Adicionando funcao objetivo */
+  for(int aux=1; aux<=data.n; aux++){
     glp_set_obj_coef(lp,aux,data.c[aux]);
   }
-  for(int i=1; i<= data.n;i++){
+
+  /* Adicionando coeficientes das restricoes */
+  for(int i=1; i<=data.n;i++){
     for(int j=1; j<=data.n;j++){
-      ia[aux]=i, ja[aux]=j , ar[aux]=data.a[i-1][j-1];
+      ia[aux] = i, ja[aux] = j, ar[aux] = data.a[i-1][j-1];
       aux++;
     }
   }
-
   glp_load_matrix(lp, aux, ia, ja, ar);
-  /* solve problem */
+
+  /* Obtencao da solucao apartir do metodo de branch and cut */
   glp_intopt(lp, NULL);
-  /* recover and display results */
+
+  /* Display dos resultados */
   z = glp_get_obj_val(lp);
   printf("função objetivo: %g\n",z);
   for(int i=1; i<=data.n;i++){
@@ -94,9 +120,10 @@ int main(void)
     printf("x%d = %d,  ",i,x[i]);
   }
 
-  /* housekeeping */
+  /* Desalocacao de memoria */
   glp_delete_prob(lp);
   glp_free_env();
   free(x);
+
   return 0;
 }
